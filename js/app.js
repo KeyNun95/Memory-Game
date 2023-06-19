61dc035844a3a7594b62bcf8a0c2aa01b66c737c
Keyla N <k.nunez2017@gmail.com>
2023-06-19 19:59:49 -0400

    Adding more

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 /*----- constants -----*/
 
 /*----- state variables -----*/
-
+//identify first card selection
+//identify second card selection
+//cards on board
 /*----- cached elements -----*/
 
 /*----- event listeners -----*/
-
+//click so cards can be selected
+//board clearedor not = result message
 /*----- functions -----*/
+//make card flip upon clicking
+
 //start button
 
 //play again button
