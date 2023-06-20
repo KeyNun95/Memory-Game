9556e1cc0c4d75a5c38ed572aec51a62b6bfc584
Keyla N <k.nunez2017@gmail.com>
2023-06-20 13:07:11 -0400

    new changes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
 /*----- constants -----*/
 const allTheCards = document.querySelector(".cards");
+const cardPictures = ['kermit', 'pikachu', 'doge', 'monkey', 'pepe', 'spongebob', 'looking-back guy', 'cat sitting at table', 'butterfly guy', 'emotional damage'];
+const cardsVariety = [...cardPictures, ...cardPictures];
+const cardsTotal = cardsVariety.length;
+
 /*----- state variables -----*/
+//build cards on board
+function fillCard(cardPicture){
+    let element = document.createElement("div");
+
+    element.classList.add("card");
+    element.setAttribute("")
+}
+//needed to loop pictures into cards
+for (let i = 0; i < cardsTotal; i++){
+    const cardPicture = cardsVariety;
+
+    console.log(cardPicture);
+}
 //identify first card selection
 //identify second card selection
-//cards on board
+
 /*----- cached elements -----*/
 
 /*----- event listeners -----*/
