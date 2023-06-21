389b2d69c5cc9608dc1a42e022da322e45811615
Keyla N <k.nunez2017@gmail.com>
2023-06-21 19:35:41 -0400

    front and back card

--- js/app.js
+++ js/app.js
@@ -1,21 +1,53 @@
 /*----- constants -----*/
 const allTheCards = document.querySelector(".cards");
 //address parent section
-const cardPictures = ['kermit', 'pikachu', 'doge', 'monkey', 'pepe', 'this is fine', 'kronk', 'cat sitting at table', 'butterfly guy', 'emotional damage'];
+const cardPictures = [
+    {name: "this is fine", 
+    src: "https://media.npr.org/assets/img/2023/01/14/this-is-fine_sq-0bd6d8072e991dc4708be3668cd480ae7df18a11-s800-c85.jpg", 
+    front: "https://w0.peakpx.com/wallpaper/302/597/HD-wallpaper-blue-abstract-background-glitter-shiny-sparkling.jpg"}, 
+    {name:"kronk",
+    src: "",
+    front:"https://w0.peakpx.com/wallpaper/302/597/HD-wallpaper-blue-abstract-background-glitter-shiny-sparkling.jpg"}, 
+    {name: "emotional damage",
+    src: "",
+    front: "https://w0.peakpx.com/wallpaper/302/597/HD-wallpaper-blue-abstract-background-glitter-shiny-sparkling.jpg"}, 
+    {name: "butterfly guy",
+    src: ,
+    front: "https://w0.peakpx.com/wallpaper/302/597/HD-wallpaper-blue-abstract-background-glitter-shiny-sparkling.jpg"}, 
+    {"why are you running",
+    front: "https://w0.peakpx.com/wallpaper/302/597/HD-wallpaper-blue-abstract-background-glitter-shiny-sparkling.jpg"}, 
+    {"doge",
+    front: "https://w0.peakpx.com/wallpaper/302/597/HD-wallpaper-blue-abstract-background-glitter-shiny-sparkling.jpg"}, 
+    {"uganda knuckles",
+    front: "https://w0.peakpx.com/wallpaper/302/597/HD-wallpaper-blue-abstract-background-glitter-shiny-sparkling.jpg"}, 
+    {"",
+    front: "https://w0.peakpx.com/wallpaper/302/597/HD-wallpaper-blue-abstract-background-glitter-shiny-sparkling.jpg"}, 
+    {'butterfly guy',
+    front: "https://w0.peakpx.com/wallpaper/302/597/HD-wallpaper-blue-abstract-background-glitter-shiny-sparkling.jpg"}, 
+    {'emotional damage',
+    front: "https://w0.peakpx.com/wallpaper/302/597/HD-wallpaper-blue-abstract-background-glitter-shiny-sparkling.jpg"}];
 const cardsVariety = [...cardPictures, ...cardPictures]; //make the images comeback to my 
 const cardsTotal = cardsVariety.length;
 
 /*----- state variables -----*/
 //build cards on board- number of cards change as cards are matched
 function fillCard(cardPicture){
-    let element = document.createElement("div");
+    let img = document.createElement("img");
     //will make new div section
-    element.classList.add("card");
-    //will make new class inside my new div
-    element.setAttribute("picture", cardPicture);
+    img.src = cardPicture.front
 
-    return element;
+    img.classList.add("card") //<img class
+    //will make new class inside my new div
+    img.addEventListener("click", function(){
+        //when button is click img.src will change to cardPicture.src
+        img.src = cardPicture.src;
+    });
+
+    return img;
 }
+
+// topOfCard(card);
+
 //needed to loop pictures into cards
 for (let i = 0; i < cardsTotal; i++){
     let randomIndex = Math.floor(Math.random() * cardsVariety.length);
@@ -35,15 +67,19 @@ for (let i = 0; i < cardsTotal; i++){
 
 /*----- event listeners -----*/
 //click so cards can be selected
-element.addEventListener('click', flip){
-    setTimeout (waitingForMatch, 500);
-}
-//board clearedor not = result message
-/*----- functions -----*/
-//make card flip upon clicking
-function flipCard() {
-    firstCard.push(cardPictures(cardPicture))
-}
+// element.addEventListener("click", function(){
+//     if (wrongCard){
+//         return; //exit function- will return back to front of card 
+//     }
+
+//     element.style.backgroundColor = cardPicture;
+// });
+// //board clearedor not = result message
+// /*----- functions -----*/
+// //make card flip upon clicking
+// function flipCard() {
+//     firstCard.push(cardPictures(cardPicture))
+// }
 //start button
 
 //play again button
